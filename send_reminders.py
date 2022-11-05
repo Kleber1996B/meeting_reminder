d92968ed9f52a0a408ace65953ad140900a2f262
Kleber Balladares <kballadares996@gmail.com>
2022-11-05 01:34:08 -0500

    main function was modified to print the reason of the error

--- send_reminders.py
+++ send_reminders.py
@@ -40,11 +40,12 @@ def main():
 
     try:
         date, title, emails = sys.argv[1].split("|")
+        print(emails)
         message = message_template(date, title)
         send_message(message, emails)
         print("Successfully sent reminders to:", emails)
     except Exception as e:
-        print("Failure to send email", file=sys.stderr)
+        print("Failure to send email due to: {}".format(e), file=sys.stderr)
 
 
 if __name__ == "__main__":
