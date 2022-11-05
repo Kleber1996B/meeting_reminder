c6e4b6b9d0baaea4ba86eef82918288660af6f94
Kleber Balladares <kballadares996@gmail.com>
2022-11-05 01:19:05 -0500

    Function send_message, and main were defined

--- send_reminders.py
+++ send_reminders.py
@@ -21,3 +21,31 @@ def dow(date):
     """create datetime.datetim.strptime object from a str provided by the user"""
     dateobj = datetime.datetime.strptime(date, r"%Y-%m-%d")
     return dateobj.strftime("%A")
+
+
+def send_message(message, emails):
+    smtp = smtp.SMTP("localhost")
+    message["From"] = "noreply@example.com"
+    for email in emails.split(","):
+        del message["To"]
+        message["To"] = email
+        smtp.send_message(message)
+    smtp.quit()
+    pass
+
+
+def main():
+    if len(sys.argv) < 2:
+        return usage()
+
+    try:
+        date, title, emails = sys.argv[1].split("|")
+        message = message_template(date, title)
+        send_message(message, emails)
+        print("Successfully sent reminders to:", emails)
+    except Exception as e:
+        print("Failure to send email", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
